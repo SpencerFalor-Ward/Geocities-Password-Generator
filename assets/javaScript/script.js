7afbfc95ed3084a3103410faca9cb9e7e058fca7
SpencerFW <srfalorward@ucdavis.edu>
2020-01-18 14:43:03 -0800

    fixed the copy to clipboard!

--- assets/javaScript/script.js
+++ assets/javaScript/script.js
@@ -66,12 +66,12 @@ console.log(this);
 // password(prompt answer)
 genEl.addEventListener("click", function(event) {
   event.preventDefault();
-  
+
   do {
     var numPick = prompt("Type a number between 8 and 128");
     var pwLength = parseInt(numPick.trim());
     console.log(pwLength);
-    
+
     if (pwLength < 8 || pwLength > 128 || pwLength === NaN) {
       alert("Please enter a number between 8 and 128");
     }
@@ -82,12 +82,12 @@ genEl.addEventListener("click", function(event) {
   var numOn = confirm("Would you like to use numbers?");
   var spclOn = confirm("Would you like to use special characters?");
   var characters = "";
-  
+
   alphLOn === true ? (characters += alphL) : "";
   alphCOn === true ? (characters += alphC) : "";
   numOn === true ? (characters += num) : "";
   spclOn === true ? (characters += spcl) : "";
-  
+
   var yourPW = document.querySelector(".textarea");
   yourPW.textContent = password(pwLength, characters);
 });
@@ -96,9 +96,9 @@ function password(l, characters) {
   var pwd = "";
   for (var i = 0; i < l; i++) {
     pwd += characters.charAt(Math.floor(Math.random() * characters.length));
-    console.log(pwd);
+    // console.log(pwd);
   }
-  
+
   return pwd;
 }
 
@@ -106,24 +106,31 @@ function password(l, characters) {
 copyEl.addEventListener("click", function(event) {
   event.preventDefault();
 
-const copyToClipboard = str => {
-  const el = document.querySelector(".textarea"); // Create a <textarea> element
-  el.value = str; // Set its value to the string that you want copied
-  el.setAttribute("readonly", ""); // Make it readonly to be tamper-proof
-  el.style.position = "absolute";
-  el.style.left = "-9999px"; // Move outside the screen to make it invisible
-  document.body.appendChild(el); // Append the <textarea> element to the HTML document
-  const selected =
-    document.getSelection().rangeCount > 0 // Check if there is any content selected previously
-      ? document.getSelection().getRangeAt(0) // Store selection if found
-      : false; // Mark as false to know no selection existed before
-  el.select(); // Select the <textarea> content
-  document.execCommand("copy"); // Copy - only works as a result of a user action (e.g. click events)
-  document.body.removeChild(el); // Remove the <textarea> element
-  if (selected) {
-    // If a selection existed before copying
-    document.getSelection().removeAllRanges(); // Unselect everything on the HTML document
-    document.getSelection().addRange(selected); // Restore the original selection
-  }
-}
+  console.log("Click");
 
+  const copyToClipboard = () => {
+    const el = document.createElement("textarea"); // Create a <textarea> element
+    el.value = document.querySelector(".textarea").textContent; // Set its value to the string that you want copied
+    el.setAttribute("readonly", ""); // Make it readonly to be tamper-proof
+    el.style.position = "absolute";
+    el.style.display = "hidden"; // Move outside the screen to make it invisible
+    document.body.appendChild(el); // Append the <textarea> element to the HTML document
+    const selected =
+      document.getSelection().rangeCount > 0 // Check if there is any content selected previously
+        ? document.getSelection().getRangeAt(0) // Store selection if found
+        : false; // Mark as false to know no selection existed before
+    el.select(); // Select the <textarea> content
+    document.execCommand("copy"); // Copy - only works as a result of a user action (e.g. click events)
+    document.body.removeChild(el); // Remove the <textarea> element
+    console.log(selected);
+    if (selected) {
+      // If a selection existed before copying
+      document.getSelection().removeAllRanges(); // Unselect everything on the HTML document
+      document.getSelection().addRange(selected); // Restore the original selection
+    }
+  };
+
+  copyToClipboard();
+  console.log(el);
+  console.log(copyEl);
+});
